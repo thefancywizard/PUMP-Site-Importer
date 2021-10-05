c30b440214b66796934030bba3adf8062cfd4dee
PMU-ops <62953125+PMU-ops@users.noreply.github.com>
2021-10-05 21:42:15 +0000

    Revise to use step query

--- import-site.php
+++ import-site.php
@@ -24,14 +24,9 @@ $owner = env('owner', NULL);
 
 if ($migrate == "true") {
     // Migrate the site from PMU API.
-    $PMU_CLASSIC_API_USER = getenv('PMU_CLASSIC_API_USER');
-    $PMU_CLASSIC_API_PASSWORD = getenv('PMU_CLASSIC_API_PASSWORD');
-    $AUTH = shell_exec("echo -ne \"$PMU_CLASSIC_API_USER:$PMU_CLASSIC_API_PASSWORD\" | base64 --wrap 0");
-    $MIGRATABLE_SITE_DATA = shell_exec("curl --header \"Content-Type: application/json\" --header \"Authorization: Basic $AUTH\" https://mu.ps-pantheon.com/api/uw/v2/site/$uuid");
-    print_r($MIGRATABLE_SITE_DATA);
-    print_r(PHP_EOL);
-    echo "Migratable site data: " . PHP_EOL;
-    print_r(json_decode($MIGRATABLE_SITE_DATA, TRUE), TRUE) . PHP_EOL;
+    print_r("Going to import queried data:" . PHP_EOL);
+    $SITE_CLASSIC_PMU_DATA = getenv('SITE_CLASSIC_PMU_DATA');
+    print_r($SITE_CLASSIC_PMU_DATA);
 }
 else {
     echo "TODO: Non migrated import" . PHP_EOL;
